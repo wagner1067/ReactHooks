2b9d134329be742c74f966fef26f8fb8c9e83b6a
wagner1067 <barra-wag@hotmail.com>
2024-02-06 21:41:09 -0300

    Usando useReducer

--- reacthooks/src/HookReducer.jsx
+++ reacthooks/src/HookReducer.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import './App.css'
 
+function reducer(state, action) {
+    switch (action.type) {
+        case "Click":
+            return {count: state.count + 1, text: !state.text}
+    }
+}
+
 function App(){
-    const [count, setCount] = useState(0)
-    const [text, setText] = useState(true)
+    
+    const [state,  dispatch] = useReducer(reducer, {
+        count: 0,
+        text: true,
+    })
+    
+    /*const [count, setCount] = useState(0)
+    const [text, setText] = useState(true) */
 
     function Counter() {
-        setCount((count)=> count+1)
+
+        dispatch({type:'Click'})
+
+        /*setCount((count)=> count+1)
         if (count%2 != 0){
             setText(true)
         } else {
             setText(false)
-        }
+        }*/
     }
 
     return (
@@ -19,10 +35,10 @@ function App(){
         <h1>Contador... </h1>
         <div className="card">
             <button onClick={Counter}>
-                A contagem é {count}
+                A contagem é {state.count}
             </button>
             <p>
-                {text == true && (
+                {state.text == true && (
                     <p> Olá </p>
                 )}
             </p>
